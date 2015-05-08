64d61d72c1f7cc73f0d06808540e512f7e61195b
Aaron Steele <aaron.steele@logos.com>
2015-05-08 15:56:20 -0700

    update home

--- Shimoov/Shimoov/Controllers/HomeController.cs
+++ Shimoov/Shimoov/Controllers/HomeController.cs
@@ -1,36 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Linq.Expressions;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace Shimoov.Controllers
 {
-	public class HomeController : Controller
+	public sealed class HomeController : Controller
 	{
+		public HomeController()
+		{
+		}
+
 		public ActionResult Index()
 		{
-			return Content("Home");
-		}
-
-		public ActionResult Experiment(int experimentId)
-		{
-			return Content(experimentId.ToString());
-		}
-
-		public ActionResult About()
-		{
-			ViewBag.Message = "Your application description page.";
-
-			return View();
-		}
-
-		public ActionResult Contact()
-		{
-			ViewBag.Message = "Your contact page.";
-
-			return View();
+			return Content("This is the root of the Shimoov service.");
 		}
 	}
 }
