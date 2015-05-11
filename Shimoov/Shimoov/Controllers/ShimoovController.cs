1870ec39cad3cc485f79e7d88e6d0b8bb66ff6e0
Aaron Steele <aaron.steele@logos.com>
2015-05-11 16:40:52 -0700

    A little bit of code clean-up post hackathon

--- Shimoov/Shimoov/Controllers/ShimoovController.cs
+++ Shimoov/Shimoov/Controllers/ShimoovController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Runtime.Remoting.Messaging;
+﻿using System.Linq;
 using System.Web.Mvc;
 using WolframAlphaNET;
 using WolframAlphaNET.Objects;
@@ -21,50 +20,28 @@ namespace Shimoov.Controllers
 		[HttpGet]
 		public ActionResult Query(string location, int salary, string destination)
 		{
-			var locationResult = queryWolfram(location);
-			var destinationResult = queryWolfram(destination);
-			return Content(locationResult);
+			var initialIndex = QueryWolfram(location);
+			var finalIndex = QueryWolfram(destination);
+			float parsedInitialIndex;
+			float parsedFinalIndex;
+			// String (Data not Available) is returned when data is unavailable from wolfram. Otherwise, a string representation of a float is returned.
+			if (!float.TryParse(initialIndex, out parsedInitialIndex) || !float.TryParse(finalIndex, out parsedFinalIndex))
+				return Content("Service Unavailable");
+			return Content(SalaryCalculation(salary, parsedInitialIndex, parsedFinalIndex).ToString());
 		}
 
-		private int salaryCalculation(int oldSalary, int coastOfLivingRatio, int newSalary)
+		private float SalaryCalculation(int oldSalary, float oldRatio, float newRatio)
 		{
-			int adjustedSalary = (oldSalary / coastOfLivingRatio) * newSalary;
+			float adjustedSalary = (oldSalary / oldRatio) * newRatio;
 			return adjustedSalary;
 		}
 
-		private string queryWolfram(string input)
+		private string QueryWolfram(string input)
 		{
 			WolframAlpha wolfram = new WolframAlpha("RQJPTG-976HAP24AX");
 			QueryResult results = wolfram.Query("cost of living index " + input);
 
-			string result = "";
-			if (results != null)
-			{
-				result = "NOTNULL";
-				if (results.Error != null)
-					result += results.Error.Message;
-				if (results.Pods != null)
-				{
-					foreach (Pod pod in results.Pods)
-					{
-						result += "POD";
-						if (pod.SubPods != null)
-						{
-							result += "SUBPOD";
-							foreach (SubPod subPod in pod.SubPods)
-							{
-								result += subPod.Title + " ";
-								result += subPod.Plaintext;
-							}
-						}
-					}
-				}
-			}
-			else
-			{
-				result = "NULL";
-			}
-			return result;
+			return results != null ? results.Pods.ToList().Last().SubPods.ToList().Last().Plaintext : "Data not Available";
 		}
 	}
 }
